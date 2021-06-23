580ab19ac9927044317dd9d24b5cc630ff78ca7e
Kosi <kosisochukwu.asuzu@e4email.net>
2021-06-23 17:20:20 +0100

    feat: add school model to school apps

--- app_school/models.py
+++ app_school/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    email = models.EmailField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+        
