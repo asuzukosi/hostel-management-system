c9390ec1416506b8154f8e9be7a8135219a1d411
Kosi <kosisochukwu.asuzu@e4email.net>
2021-06-23 18:28:32 +0100

    feat: add school admin model

--- app_school/models.py
+++ app_school/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class School(models.Model):
@@ -12,4 +13,11 @@ class School(models.Model):
     def __str__(self):
         return self.name
 
-        
+
+ADMIN_AUTH_LEVELS = [['super', 'super'], ['regular', 'regular'], ['minor', 'minor']]
+class SchoolAdmin(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    authorization_level = models.CharField(max_length=10, choices=ADMIN_AUTH_LEVELS)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
