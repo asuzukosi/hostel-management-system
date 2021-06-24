89b781c4093f1b7f605883613de6f2140c319d23
Kosi <kosisochukwu.asuzu@e4email.net>
2021-06-24 20:42:08 +0100

    feat: add dashboard page as the root homepage

--- app_dashboard/views.py
+++ app_dashboard/views.py
@@ -1,3 +1,6 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    return HttpResponse("Hello, and welcome to the school hostel management system SAAS")
--- hms/urls.py
+++ hms/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('app_dashboard.urls')),
     path('admin/', admin.site.urls),
     path('api/school/', include('app_school.api.router')),
     path('api/user/', include('app_accounts.api.router')),
