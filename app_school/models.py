e323075e7d3f4158a33d13da7ccfaeb33d7cd81f
Kosi <kosisochukwu.asuzu@e4email.net>
2021-06-25 10:09:09 +0100

    feat: add image resize feature to school and school admin profile image

--- app_school/models.py
+++ app_school/models.py
@@ -1,8 +1,10 @@
+import os
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
+from PIL import Image
     
 
 
@@ -19,6 +21,17 @@ class School(models.Model):
     def __str__(self):
         return self.name
 
+@receiver(post_save, sender=School)
+def resize_logo(sender, instance, **kwargs):
+    # resize school logo to standard 100 x 100 size once school instance has been saved
+    if instance.logo != None:
+        image = Image.open(instance.logo)
+        image.thumbnail((100, 100))
+        image.save(os.path.join("media", instance.logo.name))
+
+
+
+
 
 ADMIN_AUTH_LEVELS = [['super', 'super'], ['regular', 'regular'], ['minor', 'minor']]
 class SchoolAdmin(models.Model):
@@ -28,6 +41,10 @@ class SchoolAdmin(models.Model):
     authorization_level = models.CharField(max_length=10, choices=ADMIN_AUTH_LEVELS)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f'{self.user} of school {self.school}'
 
 @receiver(post_save, sender=SchoolAdmin)
 def post_save_receiver(sender, instance, created, **kwargs):
@@ -40,7 +57,6 @@ def post_save_receiver(sender, instance, created, **kwargs):
         user.groups.add(school_admin_group)
         user.save()
         
-
 @receiver(pre_delete, sender=SchoolAdmin)
 def remove_school_admin_group_from_user(sender, instance, **kwargs):
     """
@@ -50,3 +66,11 @@ def remove_school_admin_group_from_user(sender, instance, **kwargs):
     school_admin_group =  Group.objects.get(name="School Admin")
     user.groups.remove(school_admin_group)
     user.save()
+
+@receiver(post_save, sender=SchoolAdmin)
+def resize_profile_image(sender, instance, **kwargs):
+    # Resize profile image to 200 x 200 standard size once school admin has been saved
+    if instance.profile_image:
+        image = Image.open(instance.profile_image)
+        image.thumbnail((200, 200))
+        image.save(os.path.join("media", instance.profile_image.name))
